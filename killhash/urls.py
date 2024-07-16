7d5a9e28859d3fc2add5d38d2234abdbf9ea1ad0
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-16 12:05:13 +0300

    request a primary key like pk or cookie

--- killhash/urls.py
+++ killhash/urls.py
@@ -6,12 +6,12 @@ from django.http import HttpResponse
 def projects(request):
     return HttpResponse("Welcome to the Projects App")
 
-def project(request):
-    return HttpResponse("single project")
+def project(request,cookie):
+    return HttpResponse("single project"+" "+cookie)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', projects,name='projects'),
-    path('project/', project,name='project'),
+    path('project/<str:cookie>/', project,name='project'),
 ]
