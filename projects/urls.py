81eeebbf9302662fd89fd85b979ecda847b58d0c
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-20 08:40:14 +0300

    passign varable in templates

--- projects/urls.py
+++ projects/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
     path('project/<str:pk>/', views.project, name='project'),
-    path('', views.mani , name='mani'),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 def projects(request):
-    return render(request,'projects/projects.html')
+    msg = "you are in the project"
+    return render(request,'projects/projects.html',{'message':msg})# for adding this message to templates just create a vireable and pass the message key
 
 def project(request,pk):
     return render(request,'projects/single_project.html')
 
-def mani(request):
-    return render(request,'main.html')
+
 
