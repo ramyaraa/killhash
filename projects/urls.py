8ba71b43210b08b536bafeb36565d3a69fab9426
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-20 08:35:02 +0300

    adding templates in pass and change path of views

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
     path('project/<str:pk>/', views.project, name='project'),
+    path('', views.mani , name='mani'),
 ]
 
--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def projects(request):
-    return render(request,'projects.html')
+    return render(request,'projects/projects.html')
 
 def project(request,pk):
-    return render(request,'single_project.html')
+    return render(request,'projects/single_project.html')
+
+def mani(request):
+    return render(request,'main.html')
 
