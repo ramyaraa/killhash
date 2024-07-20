6bb1dd6e8de772c443b7b3072ac07af661a1cc5c
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-20 13:51:19 +0300

    for loop and passing dectionary for projects path

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+projectsList = [
+    {'id': 1, 'title': 'Project 1', 'description': 'This is project 1 description'},
+    {'id': 2, 'title': 'Project 2', 'description': 'This is project 2 description'},
+    {'id': 3, 'title': 'Project 3', 'description': 'This is project 3 description'},
+    #... add more projects here...
+]
 def projects(request):
     page= "projects"
     number = 11
-    context = {'page': page,'number': number}
+    context = {'page': page,'number': number,'projects': projectsList}
     return render(request,'projects/projects.html',context)
 
 def project(request,pk):
