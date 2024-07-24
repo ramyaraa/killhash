fd169669edf461f0de70a15d5dee24306e915db6
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-24 13:39:35 +0300

    adding table for admin pannel and some other changes

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class project(models.Model):
+    #create a title for the project and maximum number of title is 200 characters
+    title = models.CharField(max_length=200) 
+    #create a description for the project , null=True means that the description is empty or you can write charecters
+    # and blank=True means same , but null=True for table to know , blank=True for django to know 
+    description = models.TextField(null=True, blank=True)
+    #create a demo link for the project and maximum number of demo link is 2000 characters
+    demo_link = models.CharField(max_length=2000,null=True, blank=True)
+    source_link = models.CharField(max_length=2000,null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    #create a id for each project and it will be unique and not editable and primary key will be uuid for each project
+    id = models.UUIDField(default =uuid.uuid4, unique=True, primary_key=True, editable=False)
