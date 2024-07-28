82f19cfc0b7913140d6a0dc5f812206fade61d8c
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-28 15:34:37 +0300

    ------important change class and check box

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
   class Meta:
     model = Project
-    fields = ['title', 'description', 'demo_link', 'source_link', 'tags','featured_image']
+    fields = ['title','featured_image', 'description', 'demo_link', 'source_link', 'tags']
+    widgets = {
+      # thsi is change the ugle check box to beautefle one
+      'tags': forms.CheckboxSelectMultiple(),
+    }
+  def __init__(self, *args, **kwargs):
+    super(ProjectForm, self).__init__(*args, **kwargs)
+    self.fields['title'].widget.attrs.update({'class': 'input'})
