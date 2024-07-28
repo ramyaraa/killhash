1781f00cc8e2629bdba56e16c2ede34f76385266
ramyar mohammed <adammhamad12300@gmail.com>
2024-07-28 14:15:58 +0300

    good

--- killhash/settings.py
+++ killhash/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+o59c7-e9(nkd(sa@px5j+&&(hm+$w1(0lx0u8aicu@z-z^e_0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
